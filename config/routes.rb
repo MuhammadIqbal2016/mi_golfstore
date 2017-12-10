b164dc6760be6f42c9e427086cdd9635d91332fa
MuhammadIqbal2016 <miqbal@staff.ncirl.ie>
2017-12-10 14:52:51 +0200

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  get 'cart/index'
+
+  #get 'site/about'
+
+  #get 'site/contact'
+
+  get '/about' => 'site#about'
+  get '/contact' => 'site#contact'
+
+  get '/Admin' => 'user#admin_login'
+  get '/logout' => 'user#logout'
+
+  get '/cart' => 'cart#index'
+  get '/cart/clear' => 'cart#clearCart'
+  get '/cart/:id' => 'cart#add'
+
+  resources :items
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
